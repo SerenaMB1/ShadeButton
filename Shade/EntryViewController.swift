b19e882b67fa95002363f148420a89c14906177f
Serena Badesha <sbadesha@gmail.com>
2021-08-23 15:47:32 +0100

    Added delete button

--- Shade/EntryViewController.swift
+++ Shade/EntryViewController.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class EntryViewController: UIViewController {
-
+    @IBOutlet weak var entryTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var deleteTapped: UIBarButtonItem!
+    
+    var entry: Entry?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if entry == nil {
+            //create
+        } else{
+            
+            entryTextView.text = entry!.text
+            
+            if let dateToBeShown = entry!.date {
+                datePicker.date = dateToBeShown
+            }
+            
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
+    override func viewWillDisappear(_ animated: Bool) {
+        //Make an Entry
+        
+        if entry == nil {
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let entry = Entry(context: context)
+                entry.date = datePicker.date
+                entry.text = entryTextView.text
+                
+            }
+        
+            
+        }
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+    @IBAction func deleteTapped(_ sender: Any) {
+        if entry != nil {
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                context.delete(entry!)
+                try? context.save()
+            }
+        }
+        navigationController?.popViewController(animated: true)
     }
-    */
-
+    
 }
